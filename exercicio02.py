0bf001b337a4ab5862952fc495593745e674e8cf
Slugz-Polarr <hinokamislug@gmail.com>
2026-03-12 15:01:27 -0300

    iniciei a atividade de formatação

---
+++ exercicio02.py
@@ -0,0 +1,5 @@
+nome = (input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+serie = int(input("Digite sua serie: "))
+
+print(f"Olá, {nome}! Você tem {idade} anos e está no {serie}° ano do ensino médio.")
