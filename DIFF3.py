a2093037a969b65b7e1c94d899c2678b6d955e23
Slugz-Polarr <hinokamislug@gmail.com>
2026-03-12 19:53:31 -0300

    sincronizando com "home pc"

---
+++ DIFF3.py
@@ -0,0 +1,219 @@
+poder = int(input("Digite o poder atual: "))
+
+if poder >= 100:
+    print("Guerreiro Lendário.")
+elif poder >= 50:
+    print("Guerreiro Experiente.")
+else:
+    print("Iniciante.")
+
+print("-----------------------------------------------------------")
+
+vida = int(input("Digite a vida atual: "))
+
+if vida >= 70:
+    print("Vida alta.")
+elif vida >= 30:
+    print("Vida média.")
+else:
+    print("Vida crítica.")
+
+print("-----------------------------------------------------------")
+
+ponto = int(input("Digite sua pontuação: "))
+
+if ponto >= 1000:
+    print("Rank S.")
+elif ponto >= 500:
+    print("Rank A.")
+else:
+    print("Rank B.")
+
+print("-----------------------------------------------------------")
+
+nota = int(input("Digite sua nota: "))
+
+if nota >= 8:
+    print("Excelente.")
+elif nota >= 6:
+    print("Aprovado.")
+else:
+    print("Reprovado.")
+
+print("-----------------------------------------------------------")
+
+forma = int(input("Digite o seu poder: "))
+
+if forma >= 9000:
+    print("Forma Suprema.")
+elif forma >= 4000:
+    print("Forma Avançada.")
+else:
+    print("Forma Base.")
+
+print("-----------------------------------------------------------")
+
+temperatura = int(input("Digite a temperatura: "))
+
+if temperatura < 60:
+    print("Normal.")
+elif temperatura < 80:
+    print("Atenção.")
+else:
+    print("Superaquecendo.")
+
+print("-----------------------------------------------------------")
+
+xp = int(input("Digite seu XP: "))
+
+if xp >= 200:
+    print("Subiu 2 Níveis.")
+elif xp >= 100:
+    print("Subiu 1 Nível.")
+else:
+    print("Continue lutando.")
+
+print("-----------------------------------------------------------")
+
+missoes_concluidas = int(input("Digite a quantidade de Missões Concluídas: "))
+
+if missoes_concluidas >= 10:
+    print("Mestre Aventureiro.")
+elif missoes_concluidas >= 5:
+    print("Aventureiro.")
+else:
+    print("Iniciante.")
+
+print("-----------------------------------------------------------")
+
+velocidade_heroi = int(input("Digite sua velocidade: "))
+
+if velocidade_heroi >= 120:
+    print("Supersônico.")
+elif velocidade_heroi >= 70:
+    print("Rápido.")
+else:
+    print("Normal.")
+
+print("-----------------------------------------------------------")
+
+defesa = int(input("Digite sua defesa: "))
+
+if defesa >= 90:
+    print("Defesa Suprema.")
+elif defesa >= 50:
+    print("Defesa média.")
+else:
+    print("Defesa baixa.")
+
+print("-----------------------------------------------------------")
+
+mana_mago = int(input("Digite sua mana: "))
+
+if mana_mago >= 100:
+    print("Magia Lendária.")
+elif mana_mago >= 50:
+    print("Magia Comum.")
+else:
+    print("Sem Magia.")
+
+print("-----------------------------------------------------------")
+
+pontos_equipe = int(input("Digite a sua pontuação: "))
+
+if pontos_equipe >= 300:
+    print("Vitória Perfeita.")
+elif pontos_equipe >= 150:
+    print("Vitória.")
+else:
+    print("Derrota.")
+
+
+print("-----------------------------------------------------------")
+
+horas = int(input("Digite quantas horas você dormiu: "))
+
+if horas >= 8:
+    print("Energia total.")
+elif horas >= 6:
+    print("Energia média.")
+else:
+    print("Muito cansado.")
+
+print("-----------------------------------------------------------")
+
+poder_vilao = int(input("Digite o poder do vilão: "))
+
+if poder_vilao >= 200:
+    print("Vilão final.")
+elif poder_vilao >= 100:
+    print("Vilão forte.")
+else:
+    print("Vilão comum.")
+
+print("-----------------------------------------------------------")
+
+combate = int(input("Digite seu ataque: "))
+
+if combate >= 150:
+    print("Golpe crítico.")
+elif combate >= 80:
+    print("Golpe forte.")
+else:
+    print("Golpe fraco.")
+
+print("-----------------------------------------------------------")
+
+torneio = int(input("Digite sua pontuação: "))
+
+if torneio >= 50:
+    print("Classificado.")
+elif torneio >= 30:
+    print("Repescagem.")
+else:
+    print("Eliminado.")
+
+print("-----------------------------------------------------------")
+
+espadachim = int(input("Digite seu nível: "))
+
+if espadachim >= 10:
+    print("Mestre.")
+elif espadachim >= 5:
+    print("Intermediário.")
+else:
+    print("Aprendiz.")
+
+print("-----------------------------------------------------------")
+
+escudo = int(input("Digite a energia do seu escudo: "))
+
+if escudo >= 80:
+    print("Escudo máximo.")
+elif escudo >= 40:
+    print("Escudo ativo.")
+else:
+    print("Escudo quebrado.")
+
+print("-----------------------------------------------------------")
+
+precisao = int(input("Digite sua precisão: "))
+
+if precisao >= 90:
+    print("Acerto perfeito.")
+elif precisao >= 60:
+    print("Bom acerto.")
+else:
+    print("Errou o alvo.")
+
+print("-----------------------------------------------------------")
+
+boss_final = int(input("Digite seu poder total: "))
+
+if boss_final >= 300:
+    print("Boss derrotado.")
+elif boss_final >= 150:
+    print("Batalha difícil.")
+else:
+    print("Derrota do herói.")
+print("-----------------------------------------------------------")
