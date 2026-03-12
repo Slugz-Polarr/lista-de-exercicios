6682bac72cbe6dbea78fe6cd89e289829df2b452
Slugz-Polarr <hinokamislug@gmail.com>
2026-03-12 20:36:06 -0300

    continuação do exercicio de frase com formatação

--- exercicio02.py
+++ exercicio02.py
@@ -4,10 +4,13 @@ serie = int(input("Digite sua serie: "))
 
 print(f"Olá, {nome}! Você tem {idade} anos e está no {serie}° ano do ensino médio.")
 
-print("-------------------------------------------------------")
+print("--------------------------------------------------------------------------------------------------------------")
 
 nome_do_jogador = (input("Digite seu nome: "))
 jogo_fav = (input("Digite seu jogo favorito: "))
 qnts_horas_jogadas = int(input("Digite suas horas semanais jogadas: "))
 
 print(f"O jogador {nome_do_jogador} joga {jogo_fav} e passa cerca de {qnts_horas_jogadas} horas por semana jogando.")
+
+print("--------------------------------------------------------------------------------------------------------------")
+
