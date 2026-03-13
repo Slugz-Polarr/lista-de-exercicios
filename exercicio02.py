cc4b51d063a3a32b45085dd3acc544bd1917d9db
Slugz-Polarr <hinokamislug@gmail.com>
2026-03-13 13:59:55 -0300

    novos exercicios de formatação de variaveis adicionados

--- exercicio02.py
+++ exercicio02.py
@@ -14,3 +14,26 @@ print(f"O jogador {nome_do_jogador} joga {jogo_fav} e passa cerca de {qnts_horas
 
 print("--------------------------------------------------------------------------------------------------------------")
 
+nome2 = (input("Digite seu nome: "))
+banda = (input("Digite seu cantor ou banda favorita: "))
+musica = (input("Digite sua música favorita: "))
+
+print(f"{nome2} gosta muito da banda {banda} e sua música favorita é {musica}.")
+
+print("--------------------------------------------------------------------------------------------------------------")
+
+nome3 = (input("Digite seu nome: "))
+marca = (input("Digite a marca do seu celular: "))
+percentage = int(input("Digite sua porcentagem de bateria: "))
+
+print(f"{nome3} está usando um celular {marca} e a bateria está em {percentage}%.")
+
+print("--------------------------------------------------------------------------------------------------------------")
+
+nome4 = (input("Digite seu nome: "))
+serie_fav = (input("Digite sua serie favorita: "))
+eps_assistidos = int(input("Digite quantos episódios você ja assistiu: "))
+
+print(f"{nome4} está assistindo {serie_fav} e já viu {eps_assistidos} episódios.")
+
+print("--------------------------------------------------------------------------------------------------------------")
