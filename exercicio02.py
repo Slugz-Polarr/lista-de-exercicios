cd5fe5232aa2f2dffb1ffbc683e1ae1f29f847a8
Slugz-Polarr <hinokamislug@gmail.com>
2026-03-16 13:54:02 -0300

    Finalização da atividade de formatação de variaveis

--- exercicio02.py
+++ exercicio02.py
@@ -16,9 +16,9 @@ print("-------------------------------------------------------------------------
 
 nome2 = (input("Digite seu nome: "))
 banda = (input("Digite seu cantor ou banda favorita: "))
-musica = (input("Digite sua música favorita: "))
+musica_fav = (input("Digite sua música favorita: "))
 
-print(f"{nome2} gosta muito da banda {banda} e sua música favorita é {musica}.")
+print(f"{nome2} gosta muito da banda {banda} e sua música favorita é {musica_fav}.")
 
 print("--------------------------------------------------------------------------------------------------------------")
 
@@ -35,5 +35,59 @@ serie_fav = (input("Digite sua serie favorita: "))
 eps_assistidos = int(input("Digite quantos episódios você ja assistiu: "))
 
 print(f"{nome4} está assistindo {serie_fav} e já viu {eps_assistidos} episódios.")
+
+print("--------------------------------------------------------------------------------------------------------------")
+
+aluno_nome = (input("Digite seu nome: "))
+escola_nome = (input("Digite o nome da sua escola: "))
+materia = (input("Digite sua matéria favorita: "))
+
+print(f"{aluno_nome} estuda na escola {escola_nome} e sua matéria favorita é {materia}.")
+
+print("--------------------------------------------------------------------------------------------------------------")
+
+nome5 = (input("Digite seu nome: "))
+social = (input("Digite sua rede social favorita: "))
+seguidor = int(input("Digite sua quantidade de seguidores: "))
+
+print(f"{nome5} usa muito o {social} e possui {seguidor} seguidores.")
+
+print("--------------------------------------------------------------------------------------------------------------")
+
+nome6 = (input("Digite seu nome: "))
+filme = (input("Digite seu filme favorito: "))
+data = int(input("Digite a data de lançamento do filme: "))
+
+print(f"{nome6} gosta muito do filme {filme} lançado em {data}.")
+
+print("--------------------------------------------------------------------------------------------------------------")
+
+nome7 = (input("Digite seu nome: "))
+materia_atual = (input("Digite a matéria que estudou hoje: "))
+horas_estudo = int(input("Digite quantas horas estudou hoje: "))
+
+print(f"{nome7} estudou {materia_atual} por {horas_estudo} horas hoje.")
+
+print("--------------------------------------------------------------------------------------------------------------")
+
+nome8 = (input("Digite seu nome: "))
+esporte =(input("Digite seu esporte favorito: "))
+time = (input("Digite seu time favorito: "))
+
+print(f"{nome8} gosta de jogar {esporte} e torce para o {time}.")
+
+print("--------------------------------------------------------------------------------------------------------------")
+
+nome9 = (input("Digite seu nome: "))
+idade2 = int(input("Digite sua idade: "))
+jogo_fav2 = (input("Digite seu jogo favorito: "))
+serie_fav2 = (input("Digite sua serie favorita: "))
+musica_fav2 = (input("Digite sua música favorita: "))
+
+print(f"Olá, {nome9}!")
+print(f"Você tem {idade2} anos.")
+print(f"Seu jogo favorito é {jogo_fav2}.")
+print(f"Sua série favorita é {serie_fav2}.")
+print(f"E sua música favorita é {musica_fav2}.")
 
 print("--------------------------------------------------------------------------------------------------------------")
